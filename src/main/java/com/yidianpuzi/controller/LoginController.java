bc732bfdb2083dddb60ac6b2aaa91f4ac6d3d0f3
zhangdengbo <291861450@qq.com>
2020-12-14 20:32:16 +0800

    'test10'

--- src/main/java/com/yidianpuzi/controller/LoginController.java
+++ src/main/java/com/yidianpuzi/controller/LoginController.java
@@ -45,6 +45,7 @@ public class LoginController {
         Passport passport = passportService.getPassportById(1l);
         response.setStatus(PassportStatus.SUCCESS.getCode());
         response.setResult(passport);
+        log.info("test10");
         return  response;
     }
 
