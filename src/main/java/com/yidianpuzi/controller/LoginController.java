1f45a5422d4504f9f4dd5b6cf4ef279522a175dc
zhangdengbo <291861450@qq.com>
2020-12-15 22:20:04 +0800

    Revert "'info2'"
    This reverts commit 48c2b734214b5299b9090734d8712ed9b322b9a9.

--- src/main/java/com/yidianpuzi/controller/LoginController.java
+++ src/main/java/com/yidianpuzi/controller/LoginController.java
@@ -47,7 +47,6 @@ public class LoginController {
         response.setResult(passport);
         log.info("info1");
         log.info("info2");
-        log.info("info3");
         return  response;
     }
 
